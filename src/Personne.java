59c5098fe776bba74ba7f299acde6d2c7a9f5832
yunqiao885 <yunqiao771@gmail.com>
2021-09-28 22:32:49 +0800

    tp1

---
+++ src/Personne.java
@@ -0,0 +1,45 @@
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "etudiant")
+public class Personne implements Serializable {
+    //tous les attributs devient etre de type private
+
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+
+    public Personne(){
+
+    }
+    public Personne(String nom, String prenom, Date dateNaissance) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+    }
+    @XmlAttribute
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+}
